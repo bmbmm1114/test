6dc0aff106e5e4015b685d9a349dc72c2c044505
Jun jihun <fnjdk1@daum.net>
2016-11-26 15:30:55 +0900

    진짜된다

--- sdsd/src/sdsd/hanyong.java
+++ sdsd/src/sdsd/hanyong.java
@@ -19,12 +19,7 @@ public class hanyong {
 	}
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("�븳�슜吏�");
-		System.out.println("鍮뚭쾶�씠痢좊궡遺��븯");
-		System.out.println("�궗�옄�뒗梨꾩떇二쇱쓽");
-		System.out.println("�뀕�뀕");
-=======
+
 		System.out.println("한용짱");
 		System.out.println("빌게이츠내부하");
 		System.out.println("사자는채식주의");
@@ -32,7 +27,7 @@ public class hanyong {
 		System.out.println("공룡고기존맛");
 		hanyong so = new hanyong();
 			so.sanajong();		
->>>>>>> branch 'master' of https://github.com/bmbmm1114/test.git
+
 		
 
 	}
